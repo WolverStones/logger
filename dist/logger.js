8da139170fa4f7bbedfd94cdcb8937c43c12b196
WolverStone <topcz@post.cz>
2024-07-05 17:45:49 +0200

    Update logger.js

--- dist/logger.js
+++ dist/logger.js
@@ -3,7 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.printErr = exports.getExecutionTime = exports.timestamp = exports.endLog = exports.startLog = exports.data = exports.debug = exports.info = exports.success = exports.syserr = exports.syslog = void 0;
 const chalk_1 = __importDefault(require("chalk"));
 const moment_1 = __importDefault(require("moment"));
 const tagList = {
@@ -18,29 +17,19 @@ const tagList = {
 const longestTagLength = Math.max(...Object.values(tagList).map(t => t.length));
 const getTag = (tag) => `${tagList[tag]}${' '.repeat(longestTagLength - tagList[tag].length)}:`;
 const timestamp = () => `${chalk_1.default.cyan.bold(`[${moment_1.default.utc().format('HH:mm:ss')}]`)}`;
-exports.timestamp = timestamp;
 const syslog = (str) => console.info(`${timestamp()} ${getTag('SYSLOG')} ${str}`);
-exports.syslog = syslog;
 const syserr = (str) => console.error(`${timestamp()} ${getTag('SYSERR')} ${str}`);
-exports.syserr = syserr;
 const success = (str) => console.log(`${timestamp()} ${getTag('SUCCESS')} ${str}`);
-exports.success = success;
 const info = (str) => console.info(`${timestamp()} ${getTag('INFO')} ${str}`);
-exports.info = info;
 const debug = (str) => console.log(`${timestamp()} ${getTag('DEBUG')} ${str}`);
-exports.debug = debug;
 const data = (str) => console.log(`${timestamp()} ${getTag('DATA')} ${str}`);
-exports.data = data;
 const startLog = (identifier) => console.log(`${timestamp()} ${getTag('DEBUG')} ${chalk_1.default.greenBright('[START]')} ${identifier}`);
-exports.startLog = startLog;
 const endLog = (identifier) => console.log(`${timestamp()} ${getTag('DEBUG')} ${chalk_1.default.redBright('[ END ]')} ${identifier}`);
-exports.endLog = endLog;
 const getExecutionTime = (hrtime) => {
     const timeSinceHrMs = (process.hrtime(hrtime)[0] * 1000
         + hrtime[1] / 1000000).toFixed(2);
     return `${chalk_1.default.yellowBright((Number.parseFloat(timeSinceHrMs) / 1000).toFixed(2))} seconds (${chalk_1.default.yellowBright(timeSinceHrMs)} ms)`;
 };
-exports.getExecutionTime = getExecutionTime;
 const printErr = (err) => {
     if (!(err instanceof Error)) {
         console.error(err);
@@ -66,4 +55,17 @@ const printErr = (err) => {
         })
             .join('\n'));
 };
-exports.printErr = printErr;
+const logger = {
+    syslog,
+    syserr,
+    success,
+    info,
+    debug,
+    data,
+    startLog,
+    endLog,
+    timestamp,
+    getExecutionTime,
+    printErr
+};
+exports.default = logger;
